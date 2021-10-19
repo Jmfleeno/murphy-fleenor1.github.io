b68fe431abb8596a355c76ee4973689a53dfe3d6
Murphy Fleenor <jmfleeno@gmail.com>
2021-10-19 22:04:09 +0000

    Matchey Part 1

--- projects/matchy/data.js
+++ projects/matchy/data.js
@@ -12,27 +12,55 @@
 // Step 1 - Object Creation //////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+//create a variable named "animal" assigned to empty object
+var animal = {};
+//give object "animal" a key named "species" with a value of any animal using dot notation
+animal.species = "dog";
+//give object "animal" a key named "name" with bracket notation
+animal["name"] = "Dog";
+//give object "animal" a key named "noises" with an empty array
+animal.noises = [];
+//console.log object "animal" 
+console.log(animal);
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Array Creation ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+//create a var "noises" assigned to empty array
+var noises = [];
+//give array "noises" a value using bracket notation
+noises[0] = "bark";
+//give array "noises" another value at the beginning, using an array function
+noises.unshift("wine");
+//give array "noises" another value using bracket syntax
+noises[noises.length] = "roar";
+noises.push("bow");
+//print the length of the array "noises"
+console.log(noises.length);
+//print the last element  in the array "noises"
+console.log(noises[noises.length]);
+//print the array "noises"
+console.log(noises);
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Combining Step 1 and 2 ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+//assign array "noises" to the object "animal" key "noises"
+animal.noises = noises;
+//add another value to the array "noises" within the object "animal"
+animal["noises"].push("shout");
+//print object "animal"
+console.log(animal);
 
 /* *******************************************************************
  * Step 4 - Review
  *
  * 1. What are the different ways you can access properties on objects?
- *
+ *      - Dot notation, bracket notation, 
  * 2. What are the different ways of accessing elements on arrays?
- *
+ *      - Bracket notation
  * *******************************************************************
  */
 
@@ -49,12 +77,61 @@
 // Step 6 - A Collection of Animals //////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+//create variable "animals" assigned to an empty array
+var animals = [];
+//push object "animal" in array "animals"
+animals.push(animal);
+//print array "animals"
+console.log(animals);
+//create a object "duck" with given key/value pairs
+var duck = {
+  species: 'duck',
+  name: 'Jerome',
+  noises: ['quack', 'honk', 'sneeze', 'woosh']
+};
+//push object "duck" into array "animals"
+animals.push(duck);
+//print array "animals"
+console.log(animals);
+//create two more animal objects mimiking object "duck"
+var cat = {
+  species: 'cat',
+  name: 'kitty',
+  noises: ['hiss', 'meow']
+};
+var lion = {
+  species: 'lion',
+  name: 'Simba',
+  noises: ['roar', 'hiss']
+};
+//push new animal objects into array "animals"
+animals.push(cat);
+animals.push(lion);
+//print array "animals"
+console.log(animals);
 
 //////////////////////////////////////////////////////////////////////
 // Step 7 - Making Friends ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+//chose a data struchter for your friends list and explain choice
+//  - arrays are best when collecting lists of data
+//create an array "friends" 
+var friends = [];
+//create a function "getRandom" that takes array "animal" and returns a random value from the array
+function getRandom(arr, min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return arr[Math.floor(Math.random() * (max - min) + min)];
+};
+//push a random value from array "animals" in the array "friends" using the function "getRandom"
+friends.push(getRandom(animals, 0, animals.length-1));
+//print array "friends"
+console.log(friends);
+//add array "friends" as a property of object "duck" under the key "friends"
+duck["friends"] = friends;
+//print object "duck"
+console.log(duck);
 
 
 /**
--- projects/studies/operators.js
+++ projects/studies/operators.js
@@ -1,31 +1,37 @@
 /*
 Operators:
 
-0. exDefinition 1
+0. Operators allow you to manipulate, compare, and assigne values and variables.
 
-1. exDefinition 2
+1. There are 5 main types of operators within Javascript.
 
-2. exDefinition 3
+2. These include assignemnt operators, arithmetic operators, comparison operators, logical operators, and unary operators.
 */
 
 // 1. Assignment Operators //
 /*
-
+Assignment operators assign values to variables
+These include assignment(=), additions assignment(+=), subtraction assignment(-=), multiplication assignment(*=), division assignment(/=), remainder assignment(%=), and exponentiation assignment(**=) 
 */
 
 // 2. Arithmetic Operators //
 /*
-
+Arithmetic operators are used to pre fore arithmetic on numbers
+These include addition(+), subtraction(-), multiplication(*), exponentiation(**), division(/), remainder/modulus(%), increment(++), and decrement(—)
+The addition operator(+) and the addition assignment operator(+=) can also be used to concatenate strings
+If a string is added to a number the number becomes a string
 */
 
 // 3. Comparison Operators //
 /*
-
+Comparison operators compare two values in a variety of ways
+These include equal(==), equal value and type(===), not equal(!=), not equal value or type(!==), greater than(>), less than(<), greater than or equal to(>=), less than or equal to(<=), and ternary operator(?)
 */
 
 // 4. Logical Operators //
 /*
-
+Logical operators allow you to compare multiple things at once.
+These include logical and(&&), logical or(||), and logical not(!)
 */
 
 // 5. Unary Operators (!, typeof, -) //
